3da5d609a8c652289a34c9258715e807f16fd836
Ivan Longarai <ivanslongarai@gmail.com>
2021-09-18 12:46:41 -0300

    Defined moviment

--- snackJavaScriptGame/script.js
+++ snackJavaScriptGame/script.js
@@ -3,10 +3,13 @@ let context = canvas.getContext("2d");
 let box = 32;
 
 let snake = [];
+
 snake[0] = {
   y: 8 * box,
   x: 8 * box,
 };
+
+let direction = "right";
 
 function createBackground() {
   context.fillStyle = "lightgreen";
@@ -20,5 +23,33 @@ function createSnake() {
   }
 }
 
-createBackground();
-createSnake();
+function startGame() {
+  createBackground();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") {
+    snakeX += box;
+  } else if (direction == "left") {
+    snakeX -= box;
+  } else if (direction == "down") {
+    snakeY += box;
+  } else if (direction == "up") {
+    snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+      x: snakeX,
+      y: snakeY,
+    };
+
+    snake.unshift(newHead);
+  }
+}
+
+let game = setInterval(() => {
+  startGame();
+}, 100);
