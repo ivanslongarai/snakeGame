5324b41e9321abdc73aaa5e13b5b7b5b7014f5a0
Ivan Longarai <ivanslongarai@gmail.com>
2021-09-18 13:49:14 -0300

    Defined keydown listener

--- snackJavaScriptGame/script.js
+++ snackJavaScriptGame/script.js
@@ -23,33 +23,42 @@ function createSnake() {
   }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.code == 'ArrowRight' && direction != "left") direction = "right";
+  if (event.code == 'ArrowDown' && direction != "up") direction = "down";
+  if (event.code == 'ArrowLeft' && direction != "right") direction = "left";
+  if (event.code == 'ArrowUp' && direction != "down") direction = "up";
+}
+
 function startGame() {
+
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   createBackground();
   createSnake();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if (direction == "right") {
-    snakeX += box;
-  } else if (direction == "left") {
-    snakeX -= box;
-  } else if (direction == "down") {
-    snakeY += box;
-  } else if (direction == "up") {
-    snakeY -= box;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "down") snakeY += box;
+  if (direction == "up") snakeY -= box;
 
-    snake.pop();
+  snake.pop();
 
-    let newHead = {
-      x: snakeX,
-      y: snakeY,
-    };
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
 
-    snake.unshift(newHead);
-  }
+  snake.unshift(newHead);
 }
 
-let game = setInterval(() => {
-  startGame();
-}, 100);
+let game = setInterval(startGame, 200);
